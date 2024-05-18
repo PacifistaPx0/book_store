7e5feb859d7e90e2bc9ce44bfc4f177907b563f2
PacifistaPX0 <richieadigwe@gmail.com>
2024-05-18 17:54:46 +0100

    some more work

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index, name="index-page"),
+    path("<int:id>", views.book_details, name="book-details")
 ]
